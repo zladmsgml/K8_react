9e89df7710159fea86c4ee5ce46fbedccb13a812
zladmsgml <eunikim99@gmail.com>
2024-09-19 15:46:13 +0900

    환경설정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { IoAccessibility } from "react-icons/io5";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='bg-slate-200'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,6 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <IoAccessibility/>
         </a>
       </header>
     </div>
